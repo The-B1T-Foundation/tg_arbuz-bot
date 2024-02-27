2def6ffaf55b856067b9d3838bb20345738e71d1
b1tflyyyy <thisisandrew0110@gmail.com>
2024-02-27 12:43:29 -0500

    updated logger & root cmake, updated config utility

--- source/utils/config/config_utility.cpp
+++ source/utils/config/config_utility.cpp
@@ -2,11 +2,41 @@
 
 std::optional<AConfig> AConfig_Utility::Load_Config()
 {
-    char* tg_token = std::getenv("TG_TOKEN");
+    const char* tg_token = std::getenv("TG_TOKEN");
     if (!tg_token)
     {
         return std::nullopt;
     }
 
-    return AConfig(tg_token);
+    const char* pg_host = std::getenv("PG_HOST");
+    if (!pg_host)
+    {
+        return std::nullopt;
+    }
+
+    const char* pg_port = std::getenv("PG_PORT");
+    if (!pg_port)
+    {
+        return std::nullopt;
+    }
+
+    const char* pg_db_name = std::getenv("PG_DB_NAME");
+    if (!pg_db_name)
+    {
+        return std::nullopt;
+    }
+
+    const char* pg_user = std::getenv("PG_USER");
+    if (!pg_user)
+    {
+        return std::nullopt;
+    }
+
+    const char* pg_password = std::getenv("PG_PASSWORD");
+    if (!pg_password)
+    {
+        return std::nullopt;
+    }
+
+    return AConfig(tg_token, pg_host, pg_port, pg_db_name, pg_user, pg_password);
 }
