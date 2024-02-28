1c4427435a7f119dec9a076181ecbdf9ef0fc5a5
b1tflyyyy <thisisandrew0110@gmail.com>
2024-02-28 09:40:45 -0500

    added simple registration

--- source/view/message_handler/message_handler.cpp
+++ source/view/message_handler/message_handler.cpp
@@ -1,14 +1,22 @@
 #include "message_handler.hpp"
 
-AMessage_Handler::AMessage_Handler(TgBot::Bot& tg_bot) :
-    TG_Bot(tg_bot)
+AMessage_Handler::AMessage_Handler(TgBot::Bot& tg_bot, ADB_Controller& db_controller) :
+    TG_Bot(tg_bot), DB_Controller(db_controller)
 { }
 
 void AMessage_Handler::Handle_All_Messages(const TgBot::Message::Ptr& message)
 {
     if (message->text == "hello")
     {
-        TG_Bot.getApi().sendMessage(message->chat->id, "hello bro!");
+        if (!DB_Controller.Is_User_Exists(message->from->id))
+        {
+            DB_Controller.Create_User(AUser(message->from->id, message->from->firstName.c_str(), message->from->username.c_str()));
+            TG_Bot.getApi().sendMessage(message->chat->id, "You are unregistered!");
+
+            return;
+        }
+
+        TG_Bot.getApi().sendMessage(message->chat->id, "You are registered!");
     }
 }
 
--- source/view/message_handler/message_handler.hpp
+++ source/view/message_handler/message_handler.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
 #include <tgbot/tgbot.h>
+#include <db_controller.hpp>
+#include <user_model.hpp>
 
 class AMessage_Handler
 {
 public:
-    explicit AMessage_Handler(TgBot::Bot& tg_bot);
+    explicit AMessage_Handler(TgBot::Bot& tg_bot, ADB_Controller& db_controller);
     ~AMessage_Handler() = default;
 
     void Handle_All_Messages(const TgBot::Message::Ptr& message);
 
 private:
     TgBot::Bot& TG_Bot;
+    ADB_Controller& DB_Controller;
 };
