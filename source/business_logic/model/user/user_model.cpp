36542496c870ff5f3a8d5990b47d5dcc3a2f7263
b1tflyyyy <thisisandrew0110@gmail.com>
2024-02-29 06:40:37 -0500

    added default ctor

--- source/business_logic/model/user/user_model.cpp
+++ source/business_logic/model/user/user_model.cpp
@@ -1,5 +1,9 @@
 #include "user_model.hpp"
 
+AUser::AUser() :
+    User_Id(0), First_Name(" "), Username(" ")
+{ }
+
 AUser::AUser(std::int64_t user_id, std::string&& first_name, std::string&& username) :
     User_Id(user_id), First_Name(first_name), Username(username)
 { }
