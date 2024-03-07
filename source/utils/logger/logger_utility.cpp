8f93eaf87bfdb2ef620c7db42a0a8546688e2a94
b1tflyyyy <thisisandrew0110@gmail.com>
2024-03-07 09:16:44 -0500

    added uniform initialization

--- source/utils/logger/logger_utility.cpp
+++ source/utils/logger/logger_utility.cpp
@@ -1,19 +1,19 @@
 #include "logger_utility.hpp"
 
 // ---------------------------------------------------------------------------------------------------------------------
-void ALogger_Utility::Message(const std::string& message)
+void ALogger_Utility::Message(std::string_view message)
 {
-    const auto now = std::chrono::system_clock::now();
-    const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+    const auto now{ std::chrono::system_clock::now() };
+    const std::time_t t_c{ std::chrono::system_clock::to_time_t(now) };
 
     std::cout << "\033[1;32m|MESSAGE|\033[0m " << message << " \033[1;32m|Date ---> " << std::ctime(&t_c) << "\033[0m";
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
-void ALogger_Utility::Error(const std::string& error)
+void ALogger_Utility::Error(std::string_view error)
 {
-    const auto now = std::chrono::system_clock::now();
-    const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+    const auto now{ std::chrono::system_clock::now() };
+    const std::time_t t_c{ std::chrono::system_clock::to_time_t(now) };
 
     std::cout << "\033[1;31m|ERROR|\033[0m " << error << " \033[1;31m|Date ---> " << std::ctime(&t_c) << "\033[0m";
 }
