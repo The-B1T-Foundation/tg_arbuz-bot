bed204c17c7c6e1c81d08b133d1949c085b98705
b1tflyyyy <thisisandrew0110@gmail.com>
2024-03-07 09:12:48 -0500

    optimised code

--- source/business_logic/model/config/config_model.cpp
+++ source/business_logic/model/config/config_model.cpp
@@ -1,42 +1,42 @@
 #include "config_model.hpp"
 
 // ---------------------------------------------------------------------------------------------------------------------
-AConfig::AConfig(std::string&& tg_token, std::string&& pg_host, std::string&& pg_port, std::string&& pg_db_name, std::string&& pg_user, std::string&& pg_password) :
+AConfig::AConfig(std::string_view tg_token, std::string_view pg_host, std::string_view pg_port, std::string_view pg_db_name, std::string_view pg_user, std::string_view pg_password) :
     TG_Token(tg_token), PG_Host(pg_host), PG_Port(pg_port), PG_DB_Name(pg_db_name), PG_User(pg_user), PG_Password(pg_password)
 { }
 
 // ---------------------------------------------------------------------------------------------------------------------
-const std::string& AConfig::Get_TG_Token() const
+std::string_view AConfig::Get_TG_Token() const
 {
     return TG_Token;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
-const std::string& AConfig::Get_PG_Host() const
+std::string_view AConfig::Get_PG_Host() const
 {
     return PG_Host;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
-const std::string& AConfig::Get_PG_Port() const
+std::string_view AConfig::Get_PG_Port() const
 {
     return PG_Port;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
-const std::string& AConfig::Get_PG_DB_Name() const
+std::string_view AConfig::Get_PG_DB_Name() const
 {
     return PG_DB_Name;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
-const std::string& AConfig::Get_PG_User() const
+std::string_view AConfig::Get_PG_User() const
 {
     return PG_User;
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
-const std::string& AConfig::Get_PG_Password() const
+std::string_view AConfig::Get_PG_Password() const
 {
     return PG_Password;
 }
