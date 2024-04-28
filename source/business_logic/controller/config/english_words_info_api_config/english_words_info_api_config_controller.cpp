726fcc56f32b71f38ae857ee95503a0befa37f89
b1tflyyyy <thisisandrew0110@gmail.com>
2024-04-28 08:52:49 -0400

    added english words info config model & controller

---
+++ source/business_logic/controller/config/english_words_info_api_config/english_words_info_api_config_controller.cpp
@@ -0,0 +1,41 @@
+// MIT License
+
+// Copyright (c) 2024 The B1T Foundation
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+#include "english_words_info_api_config_controller.hpp"
+
+std::optional<AEnglish_Words_Info_API_Config> AEnglish_Words_Info_API_Config_Controller::Load_Config()
+{
+    char* api_key{ std::getenv("ENGLISH_WORDS_INFO_API_KEY") };
+    if (!api_key)
+    {
+        return std::nullopt;
+    }
+
+    char* api_host{ std::getenv("ENGLISH_WORDS_INFO_API_HOST") };
+    if (!api_host)
+    {
+        return std::nullopt;
+    }
+
+    return AEnglish_Words_Info_API_Config{ api_key, api_host };
+}
