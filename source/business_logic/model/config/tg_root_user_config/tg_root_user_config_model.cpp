06e0bbd558338faf2b5950f24b531f06f1c08723
b1tflyyyy <thisisandrew0110@gmail.com>
2024-05-03 14:35:38 -0400

    added root user config

---
+++ source/business_logic/model/config/tg_root_user_config/tg_root_user_config_model.cpp
@@ -0,0 +1,35 @@
+// MIT License
+
+// Copyright (c) 2024 The B1T Foundation
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+#include "tg_root_user_config_model.hpp"
+
+// ---------------------------------------------------------------------------------------------------------------------
+ATG_Root_User_Config::ATG_Root_User_Config(std::int64_t root_id) :
+    Root_Id{ root_id }
+{ }
+
+// ---------------------------------------------------------------------------------------------------------------------
+std::int64_t ATG_Root_User_Config::Get_Root_Id() const
+{
+    return Root_Id;
+}
